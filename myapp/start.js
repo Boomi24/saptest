d349de286b03f93c5efa86893555e178598457bb
Boomi24 <thangamarinagu@gmail.com>
2025-08-24 14:15:19 +0530

    fix: Added a new REST HTTP methods of get and post

--- myapp/start.js
+++ myapp/start.js
@@ -1,22 +1,56 @@
-const express = require("express");
-const passport = require("passport");
-const xsenv = require("@sap/xsenv");
-const { createSecurityContext, requests, constants, TokenInfo, JWTStrategy } = require("@sap/xssec").v3;
+const express = require('express');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const xsenv = require('@sap/xsenv');
+const { createSecurityContext, requests, constants, TokenInfo, JWTStrategy } = require('@sap/xssec').v3;
 
+const users = require('./users.json');
 const app = express();
 
 const services = xsenv.getServices({ uaa: 'nodeuaa' });
 
 passport.use(new JWTStrategy(services.uaa));
 
+app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.authenticate('JWT', {session:false}));
 
-app.get('/',(req,res)=>{
-    res.send("Aplication user: "+ req.user.id);
+app.get('/users',(req,res)=>{
+    try{
+        const isAuthorized = req.authInfo.checkLocalScope('Display');
+        if (isAuthorized) {
+            res.status(200).json(users);
+        } else {
+            res.status(403).send('Forbidden');
+        }
+    }
+    catch(err){
+        console.error('Error in Get /users: ', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/users',(req,res)=>{
+    try{
+        const isAuthorized = req.authInfo.checkLocalScope('Update');
+        if(!isAuthorized){
+            res.status(403).json('Forbidden');
+            return;
+        }
+        const newUser = req.body;
+        newUser.id = users.length;
+        users.push(newUser);
+
+        res.status(201).json(newUser);
+    }
+    catch(err){
+        console.error('Error in POST /users: ', err);
+        res.status(500).send('Internal Server Error');
+        
+    }
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
-    console.log("The Application is running in the port: " + port);    
+    console.log('The Application is running in the port: ' + port);    
 });
